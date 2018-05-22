2c304b2b4d4183c43097f5c44c5fe0645a168c73
Eldar Mamedov <eldar.mamedov@list.ru>
2018-05-22 11:52:59 +0300

    Add config files and project skeleton

---
+++ app/src/main/java/eim/yar/sbertask/WeatherActivity.java
@@ -0,0 +1,16 @@
+package eim.yar.sbertask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Main activity of the application.
+ */
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
